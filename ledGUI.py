770f0a6b139075c990787ed0241053135a75d24f
Michal <mziolkiewicz@edu.cdv.pl>
2020-06-14 23:26:10 +0200

    Working save to file

--- ledGUI.py
+++ ledGUI.py
@@ -13,6 +13,7 @@ wdth = 250
 bttBcg = '#686de0'
 actvBtt = '#4834d4'
 bcgColor = '#c8d6e5'
+
 conStat = test.conStat #connection status message
 
 root = Tk()
@@ -52,8 +53,6 @@ def loadColors(event):
     colorsQuantity.place(x=10, y=0, width=300, height=30, anchor='nw')
     print("Done")
     
-
-
 def onClosing(event): #not working
     print("Kill leds")
     root.destroy()
@@ -69,7 +68,6 @@ def getIndex(event):
     colSetLab = Label(rightFrame, text=comm, bg=bttBcg,bd=3 ,relief='solid', font=smallArial, fg="black")
     colSetLab.place(x=10, y=530, width=300, height=30, anchor='nw')
 
-
 def lastColor(event):
     test.loadLastColor()
 
@@ -81,6 +79,68 @@ def clearFrame():
     for l in list:
         l.place_forget()
 
+def getInptValue(event):
+    count = len(open(dir_path + '/' + filePath, "r").readlines())+1
+
+    addRed.delete(3,END)
+    addGreen.delete(3,END)
+    addBlue.delete(3,END)
+
+    red = addRed.get()
+    green = addGreen.get()
+    blue = addBlue.get()
+    hext = addHex.get() 
+    name = addName.get()
+
+    red = test.addZero(red)
+    green = test.addZero(green)
+    blue = test.addZero(blue)
+
+    rgb = red+green+blue
+
+
+    #check is digit and show info 
+    if not red.isdigit() or int(red) > 255:
+        err.place_forget()
+        print('Incorrect - red')
+        err.place(x=410, y=50, width=40, height=40, anchor='nw')
+        rgbCorrect = False
+
+    elif not green.isdigit() or int(green) > 255:
+        err.place_forget()
+        print('Incorrect - green')
+        err.place(x=410, y=120, width=40, height=40, anchor='nw')
+        rgbCorrect = False
+
+    elif not blue.isdigit() or int(blue) > 255:
+        err.place_forget()
+        print('Incorrect - blue')
+        err.place(x=410, y=190, width=40, height=40, anchor='nw')
+        rgbCorrect = False
+
+    else:
+        err.place_forget()
+        print("Value correct")
+        rgbCorrect = True
+
+
+    if len(hext) == 7 and len(name) > 0:
+        print(f'HEX value ok {hext}')
+
+    elif len(rgb) == 9 and rgb != "000000000" and rgbCorrect and len(name) > 0:
+        print(f'RGB value ok: {rgb}')
+        writable = str(count) + "." + name + "_" + rgb
+        test.writeToFile(writable)
+        #set new color checkbox
+        print("Send: " + writable)
+
+    else:
+        print(f'Wrong value: rgb-{len(rgb)}; hex-{len(hext)}; RGB digit: {rgb.isdigit()}; Name len: {len(name)}')    
+
+
+
+
+
 def addNewColor(event): #printing widgets
     clearFrame()
 
@@ -91,14 +151,16 @@ def addNewColor(event): #printing widgets
     addRed.place(x=330, y=50, width=80, height=40, anchor='nw')
     addGreen.place(x=330, y=120, width=80, height=40, anchor='nw')
     addBlue.place(x=330, y=190, width=80, height=40, anchor='nw')
-    addHex.place(x=330, y=260, width=80, height=40, anchor='nw')
+    addHex.place(x=330, y=260, width=150, height=40, anchor='nw')
+    addName.place(x=330, y=330, width=200, height=40, anchor='nw')
 
     redLab.place(x=230, y=50, width=100, height=40, anchor='nw')
     greenLab.place(x=230, y=120, width=100, height=40, anchor='nw')
     blueLab.place(x=230, y=190, width=100, height=40, anchor='nw')
     hexLab.place(x=230, y=260, width=100, height=40, anchor='nw')
+    nameLab.place(x=230, y=330, width=100, height=40, anchor='nw')
 
-    addColor.place(x=230, y=330, width=100, height=40, anchor='nw')
+    addColor.place(x=230, y=400, width=100, height=40, anchor='nw')
 
 
 canvas = Canvas(root, height=720, width=1280)
@@ -152,14 +214,17 @@ addRed = Entry(rightFrame, bd=3, relief='solid', font=sArial)
 addGreen = Entry(rightFrame, bd=3, relief='solid', font=sArial)
 addBlue = Entry(rightFrame, bd=3, relief='solid', font=sArial)
 addHex = Entry(rightFrame, bd=3, relief='solid', font=sArial)
+addName = Entry(rightFrame, bd=3, relief='solid', font=sArial)
 
 redLab = Label(rightFrame, text='Red: ', bg=bttBcg,bd=3 ,relief='solid', font=smallArial, fg="black")
 greenLab = Label(rightFrame, text='Green: ', bg=bttBcg,bd=3 ,relief='solid', font=smallArial, fg="black")
 blueLab = Label(rightFrame, text='Blue: ', bg=bttBcg,bd=3 ,relief='solid', font=smallArial, fg="black")
 hexLab = Label(rightFrame, text='Hex: ', bg=bttBcg,bd=3 ,relief='solid', font=smallArial, fg="black")
+nameLab = Label(rightFrame, text='Name: ', bg=bttBcg,bd=3 ,relief='solid', font=smallArial, fg="black")
+err = Label(rightFrame, text='!', bg='red',bd=3 ,relief='solid', font=Arial, fg="black")
 
 addColor = Button(rightFrame, text='Add', bd=3, relief='solid', font=smallArial ,fg='black')
-# addColor.bind("<Button-1>", getValue)
+addColor.bind("<Button-1>", getInptValue)
 
 
 root.mainloop()
